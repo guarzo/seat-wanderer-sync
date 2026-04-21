aca88a43e5aa84e7b4cd2aac21b84e085c13fb29
Guarzo <guarzo.eve@gmail.com>
2026-04-21 01:00:49 -0400

    feat: add plugin, permissions, sidebar config under wanderer-sync namespace

---
+++ src/Config/permissions.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'edit' => [
+        'label' => 'wanderer-sync::settings.permission_edit',
+        'description' => 'wanderer-sync::settings.permission_edit_description',
+    ],
+];
---
+++ src/Config/sidebar.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'wanderer-sync' => [
+        'label' => 'wanderer-sync::settings.sidebar',
+        'name' => 'wanderer-sync::settings.sidebar',
+        'icon' => 'fas fa-map',
+        'route' => 'wanderer-sync::settings',
+        'route_segment' => 'wanderer-sync',
+        'permission' => 'wanderer-sync.edit',
+    ],
+];
