15e9979b2920cc266e3528103271e580d64ba6c9
Guarzo <guarzo.eve@gmail.com>
2026-04-21 00:53:28 -0400

    chore: remove upstream sources ahead of fork rewrite

--- src/Config/permissions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'edit' => [
-        'label' => 'wanderer-access-sync::settings.permission_edit',
-        'description' => 'wanderer-access-sync::settings.permission_edit_description',
-    ]
-];
